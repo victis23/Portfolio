4ff89989f1aeb33fc0bc14325cbb82282240458b
Michael Wells <wow@duhmarket.com>
2020-03-28 16:58:27 -0400

    Change hello world font size

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-
-
 var requestButton = document.getElementById("RequestServicesBtn");
-requestButton.addEventListener("click", userClickedRequestBtn)
+requestButton.addEventListener("click", userClickedRequestBtn);
 
 //Executed when user taps Request Services button.
 function userClickedRequestBtn() {
    
 }
 
-
-
-
 // Just an example of how to access style attributes
- /*
+    /*
     if (requestButton.style.backgroundColor === "red") {
         requestButton.style.backgroundColor = "transparent";
     }else{
