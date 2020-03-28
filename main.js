fcbbf40373edc76b22c83585b2160c911d388471
Michael Wells <wow@duhmarket.com>
2020-03-28 16:05:03 -0400

    Add method userClickedRequestBtn

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 
 
 var requestButton = document.getElementById("RequestServicesBtn");
-requestButton.addEventListener
+requestButton.addEventListener("click", userClickedRequestBtn)
+
+//Executed when user taps Request Services button.
+function userClickedRequestBtn() {
+    /*
+    if (requestButton.style.backgroundColor === "red") {
+        requestButton.style.backgroundColor = "transparent";
+    }else{
+        requestButton.style.backgroundColor = "red";
+    }
+    */
+}
