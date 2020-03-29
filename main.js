ea9bfb6e4132273dfd733f16b096eded09f26012
Michael Wells <wow@duhmarket.com>
2020-03-28 20:33:24 -0400

    Add comment with rundown for userClickedRequestBtn method.

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ requestButton.addEventListener("click", userClickedRequestBtn);
 
 //Executed when user taps Request Services button.
 function userClickedRequestBtn() {
-   
+/* 
+Sudo code
+— When button is clicked slide in new screen with a purple / pink gradient 
+containing a contact form
+— Change gradient colors for skills to match
+— Prsent user with submit button
+— Return to main screen upon submit button click
+*/
 }
 
 // Just an example of how to access style attributes
