f691908519329278a6e25224e121d87434976bd2
Michael Wells <wow@duhmarket.com>
2020-04-05 19:29:44 -0400

    Comment out old script

--- output/EmailScript.php
+++ output/EmailScript.php
@@ -34,6 +34,16 @@ echo "Run Function";
 
 ?>
 
+<!-- For future reference -->
+
 <!-- if($_POST["message"]) {
     mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
 } -->
+
+ <!-- $pass = email($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['description']);
+
+ function email($name,$phone,$email,$message) {
+     echo($name);
+    echo("Not Working!");
+    return true;
+ } -->
