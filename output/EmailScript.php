809cc5a120e2e9d21778b6ee5add63906d12461b
Michael Wells <wow@duhmarket.com>
2020-04-05 19:27:56 -0400

    Add script for sending email to php file.

--- output/EmailScript.php
+++ output/EmailScript.php
@@ -1,13 +1,34 @@
 
 <?php 
 
-// $pass = email($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['description']);
+function SendEmail(){
 
-// function email($name,$phone,$email,$message) {
-//     echo($name);
-//     echo("Not Working!");
-//     return true;
-// }
+if (!isset($_POST["submit"])){
+    echo "Form is invalid";
+}
+
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+$description = $_POST["requestDescription"];
+
+$email_from = "wow@duhmarket.com";
+$email_subject = "Request from form";
+$email_message = "The following email was automatically generated with the following message: \n
+$name /n
+$phone /n
+$email /n
+$description .
+";
+$to = "wow@duhmarket.com";
+$headers = "From: $email_from \r\n";
+
+// Send Email: 
+mail($to,$email_subject,$email_message,$headers);
+
+echo "Run Function";
+
+}
 
 
 
