ae26a80f907e228beb56b9f942373cdf4a639ed3
Michael Wells <wow@duhmarket.com>
2020-04-05 16:08:09 -0400

    Add php file

--- output/EmailScript.php
+++ output/EmailScript.php
@@ -1,14 +1,18 @@
 
 <?php 
 
-$pass = email($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['description']);
+// $pass = email($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['description']);
 
-function email($name,$phone,$email,$message) {
-    echo($name);
-    echo("Not Working!");
-    return true;
-}
+// function email($name,$phone,$email,$message) {
+//     echo($name);
+//     echo("Not Working!");
+//     return true;
+// }
 
 
 
 ?>
+
+<!-- if($_POST["message"]) {
+    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+} -->
