9efb9e0cbc84263aee5b673eef0d3dc27b1c7409
Michael Wells <wow@duhmarket.com>
2020-04-05 22:31:38 -0400

    Remove firebase reference

--- output/firestoredb.js
+++ output/firestoredb.js
@@ -1,39 +1,26 @@
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyA8uLjvF05fH-TUjt-QRZ8pkZeFeJWVmno",
-    authDomain: "portfolio-5f956.firebaseapp.com",
-    databaseURL: "https://portfolio-5f956.firebaseio.com",
-    projectId: "portfolio-5f956",
-    storageBucket: "portfolio-5f956.appspot.com",
-    messagingSenderId: "322089138229",
-    appId: "1:322089138229:web:550438e22376920995d97c",
-    measurementId: "G-9GSC89H1B3"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-submitButton.addEventListener("click",addToDataBase);
 
-var db = firebase.firestore()
+// submitButton.addEventListener("click",addToDataBase);
 
-function addToDataBase(){
+// var db = firebase.firestore()
 
-    console.log("The firebase file is being loaded");
+// function addToDataBase(){
 
-    db.collection("users").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-    })
-        .then(function (docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function (error) {
-            console.error("Error adding document: ", error);
-        });
-}
+//     console.log("The firebase file is being loaded");
+
+//     db.collection("users").add({
+//         first: "Ada",
+//         last: "Lovelace",
+//         born: 1815
+//     })
+//         .then(function (docRef) {
+//             console.log("Document written with ID: ", docRef.id);
+//         })
+//         .catch(function (error) {
+//             console.error("Error adding document: ", error);
+//         });
+// }
 
 
 
