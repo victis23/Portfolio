0884768a76ecf758e07103377469a123c072aabb
Michael Wells <wow@duhmarket.com>
2020-04-05 20:05:39 -0400

    Add Node.js functions for firebase.

--- output/EmailScript.php
+++ output/EmailScript.php
@@ -1,38 +1,36 @@
+<html>
+    <body>
+        <?php 
+            if (!isset($_POST["submit"])){
+                echo "Form is invalid";
+            }
 
-<?php 
+            $name = $_POST["name"];
+            $phone = $_POST["phone"];
+            $email = $_POST["email"];
+            $description = $_POST["requestDescription"];
 
-function SendEmail(){
+            $email_from = "wow@duhmarket.com";
+            $email_subject = "Request from form";
+            $email_message = "The following email was automatically generated with the following message: \n
+            $name /n
+            $phone /n
+            $email /n
+            $description .
+            ";
+            $to = "wow@duhmarket.com";
+            $headers = "From: $email_from \r\n";
 
-if (!isset($_POST["submit"])){
-    echo "Form is invalid";
-}
+           
 
-$name = $_POST["name"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
-$description = $_POST["requestDescription"];
+            // Send Email: 
+            mail($to,$email_subject,$email_message,$headers);
+            header("Location: https://www.michaelwells.dev");
+            echo "Run Function";
+        ?>
+    </body>
+</html>
 
-$email_from = "wow@duhmarket.com";
-$email_subject = "Request from form";
-$email_message = "The following email was automatically generated with the following message: \n
-$name /n
-$phone /n
-$email /n
-$description .
-";
-$to = "wow@duhmarket.com";
-$headers = "From: $email_from \r\n";
-
-// Send Email: 
-mail($to,$email_subject,$email_message,$headers);
-
-echo "Run Function";
-
-}
-
-
-
-?>
 
 <!-- For future reference -->
 
