b3829f58eecc3de0aaa9eddef4e3ea4d2e2d8658
Michael Wells <wow@duhmarket.com>
2020-04-05 23:13:04 -0400

    Add iOS Application to directory.

---
+++ PortfolioAppMessages/PortfolioAppMessages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PortfolioAppMessages
+//
+//  Created by Scott Leonard on 4/5/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
