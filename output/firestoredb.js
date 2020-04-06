48001fff9dffa570aac7d780f8cb0d60f3fdc96b
Michael Wells <wow@duhmarket.com>
2020-04-05 21:40:18 -0400

    Add Node.js

--- output/firestoredb.js
+++ output/firestoredb.js
@@ -5,3 +5,11 @@ const firebase = require('firebase');
 require('firebase/firestore');
 
 console.log("The firebase file is being loaded");
+
+// var nameField = document.getElementById("NameField");
+// var phoneField = document.getElementById("PhoneNumberField");
+// var emailField = document.getElementById("EmailField");
+// var textField = document.getElementById("DescriptionField");
+
+// var submitButton = document.getElementById("SubmitButton");
+
