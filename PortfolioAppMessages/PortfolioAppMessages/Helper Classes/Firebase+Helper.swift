8459d8ccba4c3bc8ba22296502c0429cdcae67cf
Michael Wells <wow@duhmarket.com>
2020-04-06 10:46:06 -0400

    remove reference method.

--- PortfolioAppMessages/PortfolioAppMessages/Helper Classes/Firebase+Helper.swift
+++ PortfolioAppMessages/PortfolioAppMessages/Helper Classes/Firebase+Helper.swift
@@ -45,28 +45,4 @@ class FireBaseHelper {
 			})
 		}
 	}
-	
-	func get(){
-		let db = Firestore.firestore()
-		
-		let messageList = Messages()
-		db.collection("Messages").getDocuments { (snapshot, error) in
-			
-			if let items = snapshot?.documents{
-				
-				items.forEach { item in
-					
-					if let message = Message(name: item.data()["name"] as! String,
-											 phone: item.data()["phone"] as! String,
-											 email: item.data()["email"] as! String,
-											 message: item.data()["message"] as! String) {
-						messageList.messages.append(message)
-					}
-					messageList.messages.forEach{ message in
-						print(message.name)
-					}
-				}
-			}
-		}
-	}
 }
