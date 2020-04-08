de93b97711fe23cdd046c9251805fa327c97e954
Michael Wells <wow@duhmarket.com>
2020-04-08 19:40:49 -0400

    Add notifications method.

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,19 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.sendPushNotifications = functions.database.ref('/Messages/{message}').onCreate((snapshot,context) => {
+exports.sendPushNotification = functions.firestore.document("Messages/{document=**}")
+.onCreate((snapshot, context) => {
 
-    console.log(snapshot);
-    console.log(context);
+    console.debug(snapshot);
+    console.debug(context);
 
-    var topic = "/topics/sentMessages";
+    const topic = "/topics/sentMessages";
 
-    var payload = {
-        data: {
-            message : 'You recieved a new message!'
+    const payload = {
+        notification : {
+            message : 'You recieved a new message!',
         }
     }
   
-    return  admin.messaging().sendToTopic(topic,payload).then((response) => {
-        
-        return response;
-    })
+    return  admin.messaging().sendToTopic(topic,payload);
 })
