99ed51fbb8ac6a2def6ae5a992cb982bf67bc29f
Michael Wells <wow@duhmarket.com>
2020-04-07 20:47:07 -0400

    Add function to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,34 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.notifyNewMessage = functions.database.ref('Messages').onCreate((snapshot,context) => {
+    
+    const message = snapshot._data();
+    const senderName = message['name'];
+    const senderMessage = message['message'];
+
+    console.log(senderName);
+    console.log(senderMessage);
+
+    return admin.firestore().doc().get().then( userDoc => {
+
+        console.debug(userDoc);
+
+        const registrationTokens = userDoc.get('registrationTokens');
+        const notificationBody = ("You recieved a new request from https://www.michaelwells.dev");
+        
+        const payload = {
+            notification: {
+                title: senderName + " sent you a messsage.",
+                body: notificationBody,
+                clickAction: "RemoveBadge"
+            },
+            data : {
+                userName : senderName,
+                userId : message['senderId']
+            }
+        }
+        return admin.messaging().sendToDevice(registrationTokens,payload)
+    })
+})
