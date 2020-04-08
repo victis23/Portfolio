27bcc4ef283a5c80e3b2af0231973e391fbfae43
Michael Wells <wow@duhmarket.com>
2020-04-08 00:14:46 -0400

    Add server side functions

--- functions/index.js
+++ functions/index.js
@@ -3,40 +3,38 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.notifyNewMessage = functions.database.ref('/Messages/').onCreate((snapshot, context) => {
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-exports.notifyNewMessage = functions.database.ref('Messages').onCreate((snapshot,context) => {
-    
     const message = snapshot._data();
     const senderName = message['name'];
     const senderMessage = message['message'];
 
+    console.debug('This was the user auth for writer' + context.auth.uid);
+
     console.log(senderName);
     console.log(senderMessage);
 
-    return admin.firestore().doc().get().then( userDoc => {
+    return admin.firestore().doc().get().then(userDoc => {
 
         console.debug(userDoc);
 
-        const registrationTokens = userDoc.get('registrationTokens');
+        const token = "drLtG9pfFUKhoZ1DrPgdlx:APA91bH_W6kD620W0RByX_6IYYbEDjHC1pXiFzFiDtL0JoPDS82ocswZYd-ZFjiInUZ7qrSbmEkkMXi55z-2oiL05OYXetsjBVtoeP4D1hHNi8HLkODYNPTKRjt8vHrgiY30Gmho9bmc";
+
         const notificationBody = ("You recieved a new request from https://www.michaelwells.dev");
         
-        const payload = {
-            notification: {
+        // const topic = '/topics/sentMessages';
+
+        const message = {
+            data: {
                 title: senderName + " sent you a messsage.",
                 body: notificationBody,
                 clickAction: "RemoveBadge"
-            },
-            data : {
-                userName : senderName,
-                userId : message['senderId']
             }
+            // topic: {
+            //     topic: topic,
+            // }
         }
-        return admin.messaging().sendToDevice(registrationTokens,payload)
+
+        return admin.messaging().sendToDevice(token,message);
     })
 })
