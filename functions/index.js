9ac7a45eb2e7b713553f40f3a924a3ee5f6c61bc
Michael Wells <wow@duhmarket.com>
2020-04-08 18:23:20 -0400

    Add server function for sending notification.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.sendPushNotifications = functions.database.ref('/Messages/{message}').onCreate((snapshot,context) => {
+
+    console.log(snapshot);
+    console.log(context);
+
+    var topic = "/topics/sentMessages";
+
+    var payload = {
+        data: {
+            message : 'You recieved a new message!'
+        }
+    }
+  
+    return  admin.messaging().sendToTopic(topic,payload).then((response) => {
+        
+        return response;
+    })
+})
